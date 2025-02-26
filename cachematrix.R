737096aed2ae8828979ed97f4e9c8e99e42eb882
Badie-Ibrahim <badie_ibrahim@outlook.com>
2025-02-27 01:59:30 +0300

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,25 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  temp <- NULL
+  set <- function(matrix){
+    x <<- matrix
+    temp <<- NULL }
+  get <- function() x
+  setinv <- function(inv) temp <<- inv
+  getinv <- function() temp
+  list (set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message ("CASH AVAILABLE")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinv(i)
+  i
 }
